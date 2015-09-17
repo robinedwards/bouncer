4c72379fbd0a520d1b0da1154d72d5f3bace793b
Robin Edwards <robin.ge@gmail.com>
2015-09-17 14:03:06 +0100

    Move handlers into separate repo

--- main.go
+++ main.go
@@ -2,50 +2,43 @@ package main
 
 import (
 	"bouncer/abtest"
+	"bouncer/handlers"
 	"flag"
 	"github.com/gorilla/mux"
-	"github.com/unrolled/render"
 	"net/http"
-	"strconv"
 	"fmt"
 )
 
-var LiveABTests = []abtest.ABTest{}
-
-func ListABTests(w http.ResponseWriter, req *http.Request) {
-	r := render.New()
-	r.JSON(w, http.StatusOK, LiveABTests)
+type BouncerDB struct {
+	ActiveABTests []abtest.ABTest
 }
 
-func Participate(w http.ResponseWriter, req *http.Request) {
-	r := render.New()
-	uid, err := strconv.ParseInt(req.URL.Query()["uid"][0], 10, 64)
+func (db BouncerDB) GetABTests() []abtest.ABTest {
+	return db.ActiveABTests
+}
 
-	if err != nil {
-		r.JSON(w, http.StatusInternalServerError, "Can't parse uid")
-	}
+var db BouncerDB
 
-	resp := abtest.Participate(LiveABTests, uid)
+func init() {
+	db.ActiveABTests = make([]abtest.ABTest, 0)
 
-	r.JSON(w, http.StatusOK, resp)
+	// setup abtests for demo
+	db.ActiveABTests = append(db.ActiveABTests, abtest.NewABTest("test1",
+		abtest.Alternative{Name: "a", Weight: 1},
+		abtest.Alternative{Name: "b", Weight: 1}))
+
+	db.ActiveABTests = append(db.ActiveABTests, abtest.NewABTest("test2",
+		abtest.Alternative{Name: "a", Weight: 1},
+		abtest.Alternative{Name: "b", Weight: 1}))
 }
 
 func main() {
 	listenPtr := flag.String("listen", "localhost:8000", "host and port to listen on")
 	flag.Parse()
 
-	// list of live tests
-	LiveABTests = append(LiveABTests, abtest.NewABTest("test1",
-		abtest.Alternative{Name: "a", Weight: 1},
-		abtest.Alternative{Name: "b", Weight: 1}))
-
-	LiveABTests = append(LiveABTests, abtest.NewABTest("test2",
-		abtest.Alternative{Name: "a", Weight: 1},
-		abtest.Alternative{Name: "b", Weight: 1}))
-
 	router := mux.NewRouter()
-	router.HandleFunc("/", ListABTests)
-	router.HandleFunc("/participate", Participate)
+	router.HandleFunc("/", handlers.ListABTests(db))
+	router.HandleFunc("/participate", handlers.Participate(db))
 
 	http.Handle("/", router)
 
---
+++ src/bouncer/handlers/handlers.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"net/http"
+	"bouncer/abtest"
+	"github.com/unrolled/render"
+	"strconv"
+)
+
+type BouncerDB interface {
+	GetABTests() []abtest.ABTest
+}
+
+func ListABTests(db BouncerDB) func(http.ResponseWriter, *http.Request) {
+	return func (w http.ResponseWriter, req *http.Request) {
+		r := render.New()
+		r.JSON(w, http.StatusOK, db.GetABTests())
+	}
+}
+
+func Participate(db BouncerDB) func(http.ResponseWriter, *http.Request) {
+	return func (w http.ResponseWriter, req *http.Request) {
+		r := render.New()
+		uid, err := strconv.ParseInt(req.URL.Query()["uid"][0], 10, 64)
+
+		if err != nil {
+			r.JSON(w, http.StatusInternalServerError, "Can't parse uid")
+		}
+
+		resp := abtest.Participate(db.GetABTests(), uid)
+
+		r.JSON(w, http.StatusOK, resp)
+	}
+}

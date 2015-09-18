be73d5af2f898af7de9d76a4c84dbc435227385d
Robin Edwards <robin.ge@gmail.com>
2015-09-18 15:51:41 +0100

    Add feature module

---
+++ src/bouncer/feature/feature.go
@@ -0,0 +1,39 @@
+package feature
+
+import (
+	"github.com/serialx/hashring"
+)
+
+type Feature struct {
+	Name         string
+	enabled		 float32
+	ring         *hashring.HashRing
+}
+
+func NewFeature(name string, enabled float32) Feature {
+	if enabled > 1 || enabled < 0 {
+		panic("enabled must be between > 0 and <= 1")
+	}
+
+	var ring *hashring.HashRing
+
+	if enabled > 0 && enabled < 1 {
+		weights := make(map[string]int)
+
+		weights["enabled"] = int(enabled * 100)
+		weights["disabled"] = int((1 - enabled) * 100)
+		ring = hashring.NewWithWeights(weights)
+	}
+
+	return Feature{name, enabled, ring}
+}
+
+func (f Feature) IsEnabled(uid string) bool {
+	if f.ring == nil {
+		return f.enabled == 1
+	}
+
+	r, _ := f.ring.GetNode(uid)
+
+	return r == "enabled"
+}
---
+++ src/bouncer/feature_test/feature_test.go
@@ -0,0 +1,44 @@
+package feature_test
+
+import (
+	"testing"
+	"bouncer/feature"
+	"strconv"
+)
+
+func TestFeature(t *testing.T) {
+
+	f := feature.NewFeature("video", 1)
+
+	if !f.IsEnabled("23424235") {
+		t.Errorf("Should be disabled")
+	}
+}
+
+func TestFeatureDisabled(t *testing.T) {
+
+	f := feature.NewFeature("video", 0)
+
+	if f.IsEnabled("3452355") {
+		t.Errorf("Should be disabled")
+	}
+}
+
+func TestFeaturePartEnabled(t *testing.T) {
+	f := feature.NewFeature("video", 0.5)
+
+	enabled := 0
+	disabled := 0
+
+	for i := 0; i < 10; i++ {
+		if f.IsEnabled(strconv.Itoa(i)) {
+			enabled += 1
+		} else {
+			disabled += 1
+		}
+	}
+
+	if enabled == 0 || disabled == 0 {
+		t.Errorf("Should be mix of disabled and enabled")
+	}
+}
